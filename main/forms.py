5ba6cfb6b7b16fb969cf6c19d606fbc64b16b080
admin <rden2000@gmail.com>
2022-03-16 20:00:46 +0300

    Fin3

--- main/forms.py
+++ main/forms.py
@@ -2,19 +2,18 @@ from django import forms
 from .models import TaskParseLikes, TaskParseSubscribers
 
 
-class TaskParseLikesForm(MultipleForm):
+class TaskParseLikesForm(forms.ModelForm):
     class Meta:
         model = TaskParseLikes
         fields = ('instagram_user', 'quantity_posts', 'quantity_users')
 
 
-class TaskParseSubscribersForm(MultipleForm):
+class TaskParseSubscribersForm(forms.ModelForm):
     class Meta:
         model = TaskParseSubscribers
         fields = ('instagram_users', 'quantity_users')
 
 
-class TaskMultipleForm(forms.Form):
-    action = forms.CharField(max_length=60, widget=forms.HiddenInput())
+
 
 
